6d770a42dd8600c2d891bf42ffc22c26d4d002e1
Romeo Kwihangana <kwiromeo@gmail.com>
2025-05-13 13:57:00 -0400

    fix formatting amphetype/main.py

--- amphetype/main.py
+++ amphetype/main.py
@@ -1,18 +1,15 @@
+from amphetype import AmphetypeWindow, app, DB
+import sys
 
 
 def main_normal():
-  import amphetype.Amphetype as A
-  import amphetype.Config as S
-  
-  w = A.AmphetypeWindow()
-  w.show()
-  r = A.app.exec_()
-  A.DB.commit()
-  return r
+    w = AmphetypeWindow()
+    w.show()
+    r = app.exec_()
+    DB.commit()
+    return r
 
 
 def main_portable():
-  import sys
-  sys.argv.append('--local')
-  return main_normal()
-  
+    sys.argv.append("--local")
+    return main_normal()
