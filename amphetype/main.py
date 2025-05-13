128cf1277511d5e4df94c9124f61cde6f5be66d3
Romeo Kwihangana <kwiromeo@gmail.com>
2025-05-13 15:04:58 -0400

    Reverting to previous main.py
    the imports are organized in such a way that messing with the
    imports in main.py leads to the application not being able to
    start.

--- amphetype/main.py
+++ amphetype/main.py
@@ -1,15 +1,19 @@
-from amphetype import AmphetypeWindow, app, DB
-import sys
-
-
 def main_normal():
-    w = AmphetypeWindow()
+    # TODO (Romeo K. 05.13.2025): due to the imports in Amphetype.py
+    # it's better to import within the function than at the top of
+    # the function. Proceed carefully when editing imports
+    import amphetype.Amphetype as A
+    import amphetype.Config as S
+
+    w = A.AmphetypeWindow()
     w.show()
-    r = app.exec_()
-    DB.commit()
+    r = A.app.exec_()
+    A.DB.commit()
     return r
 
 
 def main_portable():
+    import sys
+
     sys.argv.append("--local")
     return main_normal()
