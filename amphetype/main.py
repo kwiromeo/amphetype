0ebcedc49b172dab7f02fb8979925d50e9accde1
Romeo Kwihangana <kwiromeo@gmail.com>
2025-05-14 12:58:18 -0400

    updated indent to 2 space to prevent large diff with current repo

--- amphetype/main.py
+++ amphetype/main.py
@@ -1,19 +1,19 @@
 def main_normal():
-    # TODO (Romeo K. 05.13.2025): due to the imports in Amphetype.py
-    # it's better to import within the function than at the top of
-    # the function. Proceed carefully when editing imports
-    import amphetype.Amphetype as A
-    import amphetype.Config as S
+  # TODO (Romeo K. 05.13.2025): due to the imports in Amphetype.py
+  # it's better to import within the function than at the top of
+  # the function. Proceed carefully when editing imports
+  import amphetype.Amphetype as A
+  import amphetype.Config as S
 
-    w = A.AmphetypeWindow()
-    w.show()
-    r = A.app.exec_()
-    A.DB.commit()
-    return r
+  w = A.AmphetypeWindow()
+  w.show()
+  r = A.app.exec_()
+  A.DB.commit()
+  return r
 
 
 def main_portable():
-    import sys
+  import sys
 
-    sys.argv.append("--local")
-    return main_normal()
+  sys.argv.append("--local")
+  return main_normal()
