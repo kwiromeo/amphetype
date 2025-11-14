249c5972d978345d78f437a5aa9fa797a885859f
Romeo Kwihangana <kwiromeo@gmail.com>
2025-11-14 11:48:48 -0500

    add powershell script to start app

---
+++ start-amphetype.ps1
@@ -0,0 +1,10 @@
+# Script to automate starting Amphetype windows.
+# This activates the environment, then start the application as a background process
+
+Write-Host "[amphetype launcher] Activating Python Environment"
+
+.\.venv\Scripts\activate
+
+Write-Host "[amphetype launcher] Start Amphetype as a background process"
+
+python .\bootstrap.py &
