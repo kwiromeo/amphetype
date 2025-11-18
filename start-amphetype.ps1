678abddb59ab3858214d621a52127eb4a67fdb25
Romeo Kwihangana <kwiromeo@gmail.com>
2025-11-18 10:34:06 -0500

    update windows start script

--- start-amphetype.ps1
+++ start-amphetype.ps1
@@ -2,9 +2,8 @@
 # This activates the environment, then start the application as a background process
 
 Write-Host "[amphetype launcher] Activating Python Environment"
-
 .\.venv\Scripts\activate
-
 Write-Host "[amphetype launcher] Start Amphetype as a background process"
-
 python .\bootstrap.py &
+Write-Host "[amphetype launcher] Deactivate Python Environment"
+deactivate
