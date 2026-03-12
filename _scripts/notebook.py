0498096df4b38d3f871c897f33b23d8dd4e49891
Romeo Kwihangana <kwiromeo@gmail.com>
2026-03-12 16:18:10 -0400

    add ability to send to clipboard

--- _scripts/notebook.py
+++ _scripts/notebook.py
@@ -7,34 +7,41 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     # Read in source data
+    from typing import List
     from itertools import combinations
     from pprint import pprint
 
     from amphetype.lesson_builder import common_words
 
-    source_words = []
+    source_words: List[str] = []
     source_words.extend(common_words.get_medium_words())
     source_words.extend(common_words.get_short_words())
 
+
+    return List, combinations, pprint, source_words
+
+
+@app.cell
+def _(List, combinations, pprint, source_words: "List[str]"):
     # Create lessons for the bottom row of qwerty keyboard layout
     bottom_row_chars = "zxcvbnm"
     pairs = list(combinations(bottom_row_chars, 2))
 
     lessons = {}
     for pair in pairs:
-      pair_words = [word for word in source_words if (pair[0] in word and pair[1] in word)]
+      pair_words: List[str] = [word for word in source_words if (pair[0] in word and pair[1] in word)]
       lessons[pair] = pair_words
 
     for pair, lesson in lessons.items():
       pprint(f"{pair}: {len(lesson)}")
-    return lessons, pprint
+    return (lessons,)
 
 
 @app.cell
-def _(lessons, pprint):
+def _(lessons):
     # Extract all trigram from common words
     import more_itertools
-
+    import pyperclip
     z_words =[]
 
     for k, v in lessons.items():
@@ -49,7 +56,8 @@ def _(lessons, pprint):
       z_only = [item for item in ngram if "z" in item]
       z_ngrams.extend(set(z_only))
 
-    pprint(z_ngrams)
+    z_ngram_lesson: str = " ".join(z_ngrams)
+    pyperclip.copy(z_ngram_lesson)
     return
 
 
